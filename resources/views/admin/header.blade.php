51a2ceb79fc96b94c7ad6e259c306522d2011b14
hemascode <hemascode1@gmail.com>
2024-02-06 15:35:21 +0530

    small changes

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -8,6 +8,7 @@
   <!-- fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i"/>
   <!-- css -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="admin/Admin Panel/vendor/bootstrap/css/bootstrap.ltr.css"/>
   <link rel="stylesheet" href="admin/Admin Panel/vendor/highlight.js/styles/github.css"/>
   <link rel="stylesheet" href="admin/Admin Panel/vendor/simplebar/simplebar.min.css"/>
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -46,6 +46,7 @@
         <script src="admin/Admin Panel/js/calendar.js"></script>
         <script src="admin/Admin Panel/js/demo.js"></script>
         <script src="admin/Admin Panel/js/demo-chart-js.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
     <!-- Mirrored from stroyka-admin.html.themeforest.scompiler.ru/variants/ltr/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 01 Feb 2024 12:16:06 GMT -->
 </html>
